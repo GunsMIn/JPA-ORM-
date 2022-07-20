09ce7892e7b7a92b5de823b79b457c43031d2c99
KimGunWoo9595 <medneter@naver.com>
2022-07-20 12:53:01 +0900

    jpql

--- jpashop/src/main/java/jpabook/jpashop/domauin/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domauin/Item.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn
 public abstract class Item {
-
+//
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
     private Long id;
